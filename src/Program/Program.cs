b4035d895b0b34677a03639004340340ae022935
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-10-22 19:40:21 -0300

    ..

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,30 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            // Ejercicio 1.
+            IFilter fg = (IFilter) new FilterGreyscale();
+            IFilter fn = (IFilter) new FilterNegative();
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            PipeSerial ps = new PipeSerial(fg, new PipeSerial(fn, new PipeNull()));
+            IPicture p1 = ps.Send(picture);
+
+            PipeSerial ps2 = (PipeSerial) ps.Next;
+            IPicture p2 = ps2.Send(p1);
+
+            PipeNull pn = (PipeNull) ps2.Next;
+            IPicture p3 = pn.Send(p2);
+
+            provider.SavePicture(p3, @"beer2.jpg");
+
+            // Ejercicio 2.
+            string baseFolder = "./trans/";
+
+            provider.SavePicture(picture, baseFolder + @"beer_base_picture.jpg");
+            provider.SavePicture(p1, baseFolder + @"beer_after_filter_negative.jpg");
+            provider.SavePicture(p2, baseFolder + @"beer_after_filter_greyscale.jpg");
         }
     }
 }
